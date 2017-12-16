27320f7cf1be436b949cce7a85d88896b64b7373
Jeremy Drake <github@jdrake.com>
2017-12-16 00:51:41 -0800

    try to get some master/slave timer action going

--- src/timer.c
+++ src/timer.c
@@ -18,28 +18,56 @@ extern void led_blink(int);
 
 #define PRIO_TIM 4
 
-void timer_interrupt (void)
+static volatile uint32_t *const SYST_CVR = (uint32_t *)0xE000E018;
+static uint32_t tim3_tick = 0;
+
+void tim3_handler (void)
 {
+	uint32_t tmp = *SYST_CVR;
 	if ((TIM3->SR & TIM_SR_UIF))
 	{
 		TIM3->SR &= ~TIM_SR_UIF;
+		tim3_tick = tmp;
 		_write("Hello\r\n", 7);
 	}
 }
+
+extern void put_int (uint32_t);
+
+void tim4_handler (void)
+{
+	uint32_t tmp = *SYST_CVR;
+	if ((TIM4->SR & TIM_SR_UIF))
+	{
+		TIM4->SR &= ~TIM_SR_UIF;
+		_write("Hi\r\n", 4);
+		put_int(tmp - tim3_tick);
+	}
+}
+
+static chopstx_intr_t tim3_interrupt, tim4_interrupt;
+static struct chx_poll_head *const tim_poll[] = {
+  (struct chx_poll_head *const)&tim3_interrupt,
+  (struct chx_poll_head *const)&tim4_interrupt
+};
+#define TIM_POLL_NUM (sizeof (tim_poll)/sizeof (struct chx_poll_head *))
+
 
 
 static void *
 tim_main (void *arg)
 {
-	chopstx_intr_t interrupt;
 
 	(void)arg;
 	chopstx_usec_wait(250*1000);
 	led_blink(2);
-	chopstx_claim_irq (&interrupt, TIM3_IRQ);
+	chopstx_claim_irq (&tim3_interrupt, TIM3_IRQ);
+	chopstx_claim_irq (&tim4_interrupt, TIM4_IRQ);
 	TIM3->EGR = TIM_EGR_UG;
 	TIM3->SR = 0;
 	TIM3->DIER = TIM_DIER_UIE;
+	TIM4->SR = 0;
+	TIM4->DIER = TIM_DIER_UIE;
 	_write("Here\r\n",6);
 	TIM3->CR1 |= TIM_CR1_CEN;
 
@@ -53,8 +81,15 @@ tim_main (void *arg)
 #else
 	while (1)
 	{
-		chopstx_intr_wait (&interrupt);
-		timer_interrupt ();
+		chopstx_poll (NULL, TIM_POLL_NUM, tim_poll);
+		if (tim3_interrupt.ready)
+		{
+			tim3_handler ();
+		}
+		if (tim4_interrupt.ready)
+		{
+			tim4_handler ();
+		}
 	}
 #endif
 	return NULL;
@@ -63,17 +98,26 @@ tim_main (void *arg)
 void
 timer_init(void)
 {
-	RCC->APB1RSTR = RCC_APB1RSTR_TIM3RST;
+	RCC->APB1RSTR = RCC_APB1RSTR_TIM3RST|RCC_APB1RSTR_TIM4RST;
 	RCC->APB1RSTR = 0;
-	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN|RCC_APB1ENR_TIM4EN;
 
 	TIM3->CR1 = TIM_CR1_URS | TIM_CR1_ARPE;
+	TIM3->CR2 = TIM_CR2_MMS_1;
 	TIM3->SMCR = 0;
 	/* slow this puppy down */
 	TIM3->PSC = 36000 - 1; /* 2 kHz */
 	TIM3->ARR = 10000; /* 5s */
 	/* Generate UEV to upload PSC and ARR */
 	TIM3->EGR = TIM_EGR_UG;
+
+	TIM4->CR1 = TIM_CR1_URS | TIM_CR1_ARPE | TIM_CR1_OPM;
+	TIM4->SMCR = TIM_SMCR_TS_1 | TIM_SMCR_SMS_1|TIM_SMCR_SMS_2;
+	// TODO input capture setup
+	TIM4->PSC = 0; /* 72 MHz */
+	TIM4->ARR = 0xFFFF; /* 0.9102083 ms */
+	/* Generate UEV to upload PSC and ARR */
+	TIM4->EGR = TIM_EGR_UG;
 
 	chopstx_create (PRIO_TIM, STACK_ADDR_TIM, STACK_SIZE_TIM, tim_main, NULL);
 }
