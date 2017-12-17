d8d6ca70774725a93d2a98d2a3caa4c1fb876aaa
Jeremy Drake <github@jdrake.com>
2017-12-16 22:35:21 -0800

    try some GPIO/DMA tricks

--- src/timer.c
+++ src/timer.c
@@ -9,7 +9,45 @@
 #include "mcu/stm32f103.h"
 
 extern void _write (const char *s, int len);
+extern void put_int (uint32_t);
 extern void led_blink(int);
+
+/* why is this not in the stm32f103.h ? */
+struct GPIO {
+  volatile uint32_t CRL;
+  volatile uint32_t CRH;
+  volatile uint32_t IDR;
+  volatile uint32_t ODR;
+  volatile uint32_t BSRR;
+  volatile uint32_t BRR;
+  volatile uint32_t LCKR;
+};
+
+#define GPIOA_BASE	(APB2PERIPH_BASE + 0x0800)
+#define GPIOA		((struct GPIO *) GPIOA_BASE)
+#define GPIOB_BASE	(APB2PERIPH_BASE + 0x0C00)
+#define GPIOB		((struct GPIO *) GPIOB_BASE)
+#define GPIOC_BASE	(APB2PERIPH_BASE + 0x1000)
+#define GPIOC		((struct GPIO *) GPIOC_BASE)
+#define GPIOD_BASE	(APB2PERIPH_BASE + 0x1400)
+#define GPIOD		((struct GPIO *) GPIOD_BASE)
+#define GPIOE_BASE	(APB2PERIPH_BASE + 0x1800)
+#define GPIOE		((struct GPIO *) GPIOE_BASE)
+
+#define DMA1_ChannelN_BASE(n)    (AHBPERIPH_BASE + 0x0008 + 20*(n-1))
+#define Define_DMA1_ChannelN(n) \
+	static struct DMA_Channel *const DMA1_Channel##n = \
+	(struct DMA_Channel *)DMA1_ChannelN_BASE(n)
+
+Define_DMA1_ChannelN(2);
+Define_DMA1_ChannelN(3);
+Define_DMA1_ChannelN(4);
+Define_DMA1_ChannelN(5);
+Define_DMA1_ChannelN(6);
+Define_DMA1_ChannelN(7);
+
+#undef Define_DMA1_ChannelN
+
 
 #define STACK_PROCESS_6
 #include "stack-def.h"
@@ -17,8 +55,6 @@ extern void led_blink(int);
 #define STACK_SIZE_TIM (sizeof process6_base)
 
 #define PRIO_TIM 4
-
-extern void put_int (uint32_t);
 
 void tim3_handler (void)
 {
@@ -49,7 +85,6 @@ static struct chx_poll_head *const tim_poll[] = {
 #define TIM_POLL_NUM (sizeof (tim_poll)/sizeof (struct chx_poll_head *))
 
 
-
 static void *
 tim_main (void *arg)
 {
@@ -59,11 +94,13 @@ tim_main (void *arg)
 	led_blink(2);
 	chopstx_claim_irq (&tim3_interrupt, TIM3_IRQ);
 	chopstx_claim_irq (&tim4_interrupt, TIM4_IRQ);
-	TIM3->EGR = TIM_EGR_UG;
 	TIM3->SR = 0;
-	TIM3->DIER = TIM_DIER_UIE;
+	TIM3->DIER = TIM_DIER_UDE|TIM_DIER_UIE;
 	TIM4->SR = 0;
-	TIM4->DIER = TIM_DIER_UIE;
+	TIM4->DIER = TIM_DIER_UDE|TIM_DIER_UIE;
+	DMA1->IFCR = 0x0fffffff;
+	DMA1_Channel3->CCR |= DMA_CCR1_EN;
+	DMA1_Channel7->CCR |= DMA_CCR1_EN;
 	_write("Here\r\n",6);
 	TIM3->CR1 |= TIM_CR1_CEN;
 
@@ -91,12 +128,37 @@ tim_main (void *arg)
 	return NULL;
 }
 
+static uint32_t gpio_start_val, gpio_reset_val;
+
 void
 timer_init(void)
 {
+	gpio_start_val = (0xF << 6);
+	gpio_reset_val = (0xF << (6+16));
 	RCC->APB1RSTR = RCC_APB1RSTR_TIM3RST|RCC_APB1RSTR_TIM4RST;
 	RCC->APB1RSTR = 0;
 	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN|RCC_APB1ENR_TIM4EN;
+
+	RCC->APB2RSTR = RCC_APB2RSTR_IOPBRST;
+	RCC->APB2RSTR = 0;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+
+	RCC->AHBENR |= RCC_AHBENR_DMA1EN;
+	DMA1_Channel3->CCR = DMA_CCR1_DIR | DMA_CCR1_CIRC | DMA_CCR1_PSIZE_1 | DMA_CCR1_MSIZE_1 | DMA_CCR1_PL;
+	DMA1_Channel3->CNDTR = 1;
+	DMA1_Channel3->CPAR = (uint32_t)&GPIOB->BSRR;
+	DMA1_Channel3->CMAR = (uint32_t)&gpio_start_val;
+	DMA1_Channel7->CCR = DMA_CCR1_DIR | DMA_CCR1_CIRC | DMA_CCR1_PSIZE_1 | DMA_CCR1_MSIZE_1 | DMA_CCR1_PL;
+	DMA1_Channel7->CNDTR = 1;
+	DMA1_Channel7->CPAR = (uint32_t)&GPIOB->BSRR;
+	DMA1_Channel7->CMAR = (uint32_t)&gpio_reset_val;
+
+
+	GPIOB->BSRR = gpio_start_val;
+	/* The docs are unclear here.  In fact they say something that doesn't seem to make sense (emulate AFI by putting in AFO) */
+	/* going to try putting in ouput open drain */
+	GPIOB->CRH = (GPIOB->CRH & ~0xFF) | 0x33;
+	GPIOB->CRL = (GPIOB->CRL & ~0xFF000000) | 0x33000000;
 
 	TIM3->CR1 = TIM_CR1_URS | TIM_CR1_ARPE;
 	TIM3->CR2 = TIM_CR2_MMS_1;
