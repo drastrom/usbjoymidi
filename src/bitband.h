9154557b649d4f683080585b9a0e23da3ad78334
Jeremy Drake <github@jdrake.com>
2018-09-21 17:18:33 -0700

    Do arithmetic on uint8_t* instead of void*.
    Apparently, arithmetic on void* is undefined in the standard, and is a
    gcc extension.  I coulda sworn I've been doing that forever, in many
    different compilers.  Go figure.

--- src/bitband.h
+++ src/bitband.h
@@ -1,3 +1,3 @@
 extern uint32_t _bitband_address_out_of_range;
-#define BITBAND_SRAM(x) (((void*)(x) >= (void*)0x20000000 && (void*)(x) < (void*)0x20100000) ? (volatile uint32_t*)((void*)0x22000000 + ((void*)(x) - (void*)0x20000000)*32) : (volatile uint32_t*)&_bitband_address_out_of_range[(x)])
-#define BITBAND_PERIPH(x) (((void*)(x) >= (void*)0x40000000 && (void*)(x) < (void*)0x40100000) ? (volatile uint32_t*)((void*)0x42000000 + ((void*)(x) - (void*)0x40000000)*32) : (volatile uint32_t*)&_bitband_address_out_of_range[(x)])
+#define BITBAND_SRAM(x) (((uint8_t*)(x) >= (uint8_t*)0x20000000 && (uint8_t*)(x) < (uint8_t*)0x20100000) ? (volatile uint32_t*)((uint8_t*)0x22000000 + ((uint8_t*)(x) - (uint8_t*)0x20000000)*32) : (volatile uint32_t*)&_bitband_address_out_of_range[(x)])
+#define BITBAND_PERIPH(x) (((uint8_t*)(x) >= (uint8_t*)0x40000000 && (uint8_t*)(x) < (uint8_t*)0x40100000) ? (volatile uint32_t*)((uint8_t*)0x42000000 + ((uint8_t*)(x) - (uint8_t*)0x40000000)*32) : (volatile uint32_t*)&_bitband_address_out_of_range[(x)])
