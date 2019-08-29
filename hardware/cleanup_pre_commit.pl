e8257e4a8cb0b785cf1973b97a8e4da1f6cd0acc
Jeremy Drake <github@jdrake.com>
2019-08-28 18:04:27 -0700

    update cleanup_pre_commit.pl to latest version

--- hardware/cleanup_pre_commit.pl
+++ hardware/cleanup_pre_commit.pl
@@ -5,6 +5,6 @@ use warnings;
 
 while(<>)
 {
-	s{(path=").*/((?:fritzing-parts|partfactory|Fritzing/parts/user)/)}{$1$2}g;
+	s{(path=").*/((?:fritzing-parts|partfactory|Fritzing/parts/(?:user|contrib))/)}{$1$2}g;
 	print $_;
 }
